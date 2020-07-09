0c30fedf4df5783a79b5251764e32ed547008b12
Diogo Caldas <diogovdcpa@gmail.com>
2020-07-09 17:16:32 -0300

    update

--- API/index.js
+++ API/index.js
@@ -1,10 +1,33 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const router = express.Router();
+const fs = require('fs')
+
+app.use(express.json());
+app.use(router)
+
+const readFile = ()=>{
+    const content = fs.readFileSync('./data/base.json','utf-8')
+    return content
+}
 
 app.get("/",(req,res)=>{
+    const content = readFile();
+    res.send(content)
+});
+
+app.post("/",(req,res)=>{
+    res.send("Ola Mundo");
+});
+
+app.put("/",(req,res)=>{
+    res.send("Ola Mundo");
+});
+
+app.delete("/",(req,res)=>{
     res.send("Ola Mundo");
 });
 
 app.listen(3000,()=>{
-    console.log("Server API")
+    console.log("Servidor Rodando") 
 })
