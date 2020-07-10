e8991c64913836ed769bcd7c7ea196a8bf5ebf89
Diogo Caldas <diogovdcpa@gmail.com>
2020-07-10 13:42:02 -0300

    up date

--- API/index.js
+++ API/index.js
@@ -1,33 +1,49 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
-const fs = require('fs')
+const express = require("express")
+const server = express()
+const router = express.Router()
+const fs = require('fs');
 
-app.use(express.json());
-app.use(router)
+server.use(express.json({extended:true}));
 
 const readFile = ()=>{
     const content = fs.readFileSync('./data/base.json','utf-8')
-    return content
+    return JSON.parse(content)
 }
 
-app.get("/",(req,res)=>{
+const writeFile = (content)=>{
+    const updateFile = JSON.stringify(content)
+    fs.writeFileSync('./data/base.json',updateFile,'utf-8')
+}
+
+router.get("/",(req,res)=>{
     const content = readFile();
     res.send(content)
 });
 
-app.post("/",(req,res)=>{
+router.post("/",(req,res)=>{
+    const {nota,fornecedor,valor,reponsavel} = req.body;
+    const currentContent = readFile();
+    const id = Math.random().toString(32).substr(2,9)
+    currentContent.push({id,nota,fornecedor,valor,reponsavel})
+    writeFile(currentContent)
+    res.send({id,nota,fornecedor,valor,reponsavel})
+});
+
+router.put("/:id",(req,res)=>{
+    const {nota,fornecedor,valor,reponsavel} = req.body;
+    const {id} = req.params;
+    const currentContent = readFile();
+    const selectedItem = currentContent.findIndex((item)=>item.id === id)
+    currentContent[selectedItem] = {id,nota,fornecedor,valor,reponsavel}
+    res.send(currentContent);
+});
+
+router.delete("/",(req,res)=>{
     res.send("Ola Mundo");
 });
 
-app.put("/",(req,res)=>{
-    res.send("Ola Mundo");
-});
+server.use(router)
 
-app.delete("/",(req,res)=>{
-    res.send("Ola Mundo");
-});
-
-app.listen(3000,()=>{
+server.listen(3000,()=>{
     console.log("Servidor Rodando") 
 })
